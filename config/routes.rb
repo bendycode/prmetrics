5f5a40d0adf36ee23bf74ada23fc339b761e62d1
bendycode <stephen@bendyworks.com>
2024-10-12 16:49:44 -0500

    rails generate controller per claude

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'pull_request_users/index'
+  get 'pull_request_users/show'
+  get 'reviews/index'
+  get 'reviews/show'
+  get 'pull_requests/index'
+  get 'pull_requests/show'
+  get 'repositories/index'
+  get 'repositories/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
