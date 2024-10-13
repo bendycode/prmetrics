f6dcfd95e38405d0be1267d7d56c6637de44cf28
bendycode <stephen@bendyworks.com>
2024-10-13 10:35:32 -0500

    time_to_first_review

--- app/models/pull_request.rb
+++ app/models/pull_request.rb
@@ -7,4 +7,11 @@ class PullRequest < ApplicationRecord
   validates :number, presence: true
   validates :title, presence: true
   validates :state, presence: true
+
+  def time_to_first_review
+    first_review = reviews.order(:gh_submitted_at).first
+    return nil unless first_review && ready_for_review_at
+
+    first_review.gh_submitted_at - ready_for_review_at
+  end
 end
