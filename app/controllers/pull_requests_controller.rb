1c8635becf2b01e625b06d21deac30d40f053e1e
bendycode <stephen@bendyworks.com>
2024-10-13 20:44:58 -0500

    Show more Review info

--- app/controllers/pull_requests_controller.rb
+++ app/controllers/pull_requests_controller.rb
@@ -7,6 +7,7 @@ class PullRequestsController < ApplicationController
   def show
     @pull_request = PullRequest.find(params[:id])
     @reviews = @pull_request.reviews.includes(:author).ordered
+    @first_review_time = @reviews.last&.submitted_at
     @pull_request_users = @pull_request.pull_request_users.page(params[:page]).per(10)
   end
 end
